5036a118d11cd15f87cf0cd1336beae3045b1397
heifade <heifade@126.com>
2018-06-08 18:02:17 +0800

    no message

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-console.log(111);
+
+
+import commander from "commander";
+
+commander.version();
+
+commander.command("run [name]").action(() => {
+  console.log(1, name);
+}).parse(process.argv);
+
+
+
+console.log(11, process.argv);
