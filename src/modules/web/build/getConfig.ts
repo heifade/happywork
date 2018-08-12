ffb1b80da1e8f26bc83fdf51fffc654d0c16e36e
heifade <heifade@126.com>
2018-08-12 21:20:11 +0800

    no message

--- src/modules/web/build/getConfig.ts
+++ src/modules/web/build/getConfig.ts
@@ -24,54 +24,54 @@ export default async function(): Promise<Configuration> {
         }
       })
     ],
-    splitChunks: {
-      chunks: "all",
-      // automaticNameDelimiter: "_",
-      // minSize: 30000,
-      // minChunks: 1,
-      // maxAsyncRequests: 5,
-      // maxInitialRequests: 3,
-      name: true,
-      cacheGroups: {
-        default: {
-          minChunks: 2,
-          priority: 0, // 优先级
-          reuseExistingChunk: true
-        },
-        // 将node_modules下 异步加载的模块打包到 vendor-async.js 里
-        vendor_async: {
-          test: /node_modules/,
-          chunks: "async",
-          name: "vendor-async",
-          priority: 10, // 优先级
-          enforce: true // 强制执行(即使没有达到大小)
-        },
-        // 将node_modules下 非异步加载的模块打包到 vendor-initial.js 里
-        vendor_init: {
-          test: /node_modules/,
-          chunks: "initial",
-          name: "vendor-initial",
-          priority: 10, // 优先级
-          enforce: true // 强制执行(即使没有达到大小)
-        },
-        // 将 异步加载的模块打包到 commons-async.js 里
-        commons_async: {
-          name: "commons-async",
-          chunks: "async",
-          // minChunks: 2
-          enforce: true // 强制执行(即使没有达到大小)
-        },
-        commons_init: {
-          name: "commons-init",
-          chunks: "initial",
-          enforce: true // 强制执行(即使没有达到大小)
-          // minChunks: 2
-        }
-      }
-    },
-    runtimeChunk: {
-      name: entrypoint => `runtime-${entrypoint.name}`
-    }
+    // splitChunks: {
+    //   chunks: "all",
+    //   // automaticNameDelimiter: "_",
+    //   // minSize: 30000,
+    //   // minChunks: 1,
+    //   // maxAsyncRequests: 5,
+    //   // maxInitialRequests: 3,
+    //   name: true,
+    //   cacheGroups: {
+    //     default: {
+    //       minChunks: 2,
+    //       priority: 0, // 优先级
+    //       reuseExistingChunk: true
+    //     },
+    //     // 将node_modules下 异步加载的模块打包到 vendor-async.js 里
+    //     vendor_async: {
+    //       test: /node_modules/,
+    //       chunks: "async",
+    //       name: "vendor-async",
+    //       priority: 10, // 优先级
+    //       enforce: true // 强制执行(即使没有达到大小)
+    //     },
+    //     // 将node_modules下 非异步加载的模块打包到 vendor-initial.js 里
+    //     vendor_init: {
+    //       test: /node_modules/,
+    //       chunks: "initial",
+    //       name: "vendor-initial",
+    //       priority: 10, // 优先级
+    //       enforce: true // 强制执行(即使没有达到大小)
+    //     },
+    //     // 将 异步加载的模块打包到 commons-async.js 里
+    //     commons_async: {
+    //       name: "commons-async",
+    //       chunks: "async",
+    //       // minChunks: 2
+    //       enforce: true // 强制执行(即使没有达到大小)
+    //     },
+    //     commons_init: {
+    //       name: "commons-init",
+    //       chunks: "initial",
+    //       enforce: true // 强制执行(即使没有达到大小)
+    //       // minChunks: 2
+    //     }
+    //   }
+    // },
+    // runtimeChunk: {
+    //   name: entrypoint => `runtime-${entrypoint.name}`
+    // }
   };
 
   return config;
--- src/modules/web/configs/babel/babel.config.ts
+++ src/modules/web/configs/babel/babel.config.ts
@@ -17,22 +17,22 @@ export function getBabelConfig(modules: any) {
     ],
     cacheDirectory: resolve(process.cwd(), "build-temp/babel"),
     plugins: [
-      // [
-      //   getToolsModulePath("babel-plugin-transform-runtime"),
-      //   {
-      //     // helpers: false,
-      //     polyfill: false,
-      //     regenerator: true,
-      //     moduleName: "babel-runtime"
-      //   }
-      // ],
+      [
+        getToolsModulePath("babel-plugin-transform-runtime"),
+        {
+          helpers: false,
+          polyfill: false,
+          regenerator: true,
+          moduleName: "babel-runtime"
+        }
+      ],
+
       getToolsModulePath("babel-plugin-syntax-dynamic-import"),
       getToolsModulePath("babel-plugin-transform-es3-member-expression-literals"),
       getToolsModulePath("babel-plugin-transform-es3-property-literals"),
       getToolsModulePath("babel-plugin-transform-object-assign"),
       getToolsModulePath("babel-plugin-transform-class-properties"),
       getToolsModulePath("babel-plugin-transform-object-rest-spread")
-      
     ]
   };
 }
