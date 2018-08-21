a17f97a2ec1b58e3a4404e93b682a7c31481241c
heifade <heifade@126.com>
2018-08-21 15:49:26 +0800

    no message

--- src/modules/web/configs/webpackConfig.ts
+++ src/modules/web/configs/webpackConfig.ts
@@ -15,9 +15,7 @@ interface ReturnData {
 export async function getWebpackConfig(mode: "development" | "production"): Promise<ReturnData> {
   let webConfig = await getWebConfig(resolve(process.cwd(), "./webConfig.ts"));
 
-  let modules = false;
-
-  let babelConfig = getBabelConfig(modules || false);
+  let babelConfig = getBabelConfig(false);
   const CWD = process.cwd();
 
   let config: Configuration = {
@@ -28,7 +26,7 @@ export async function getWebpackConfig(mode: "development" | "production"): Prom
       filename: "[name].[hash:8].js",
       chunkFilename: "[name].[chunkhash:8].js"
     },
-    devtool: "source-map",
+    devtool: webConfig.sourceMap ? "source-map" : undefined,
     resolve: {
       modules: [join(__dirname, "../../../../node_modules"), join(CWD, "./node_modules")],
       extensions: [".ts", ".tsx", ".js", ".jsx", ".json"]
