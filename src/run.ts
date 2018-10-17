c07ef42cd07f186fb11a32d0a4299148d7d01584
heifade <heifade@126.com>
2018-10-17 17:48:38 +0800

    no message

--- src/run.ts
+++ src/run.ts
@@ -1,8 +1,6 @@
 const { commander } = require("../dist-core");
-import { addRenameCommand } from "./modules/rename/renameCommand";
 import { addCommand } from "./modules/web/index";
 
-addRenameCommand();
 addCommand();
 
 commander.parse(process.argv);
